cb813b78ab2ebe1a2e2d09fe0d714e9a2170ee06
EXsunshine <105021057@live.asia.edu.tw>
2016-09-19 16:27:37 +0800

    hw ok

--- week01_20160919/src/hw/hw02_105021057.java
+++ week01_20160919/src/hw/hw02_105021057.java
@@ -10,7 +10,16 @@ public class hw02_105021057 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		int n = scn.nextInt();
+		int v1 = rnd.nextInt(n)+1;
+		int v2 = rnd.nextInt(n)+1;
+		int v3 = rnd.nextInt(n)+1;
+		int v4 = rnd.nextInt(n)+1;
+		int v5 = rnd.nextInt(n)+1;
+		int v6 = rnd.nextInt(n)+1;
+		System.out.println(v1+" "+v2+" "+v3+" "+v4+" "+v5+" "+v6);
 	}
 
 }
--- week01_20160919/src/hw/hw03_105021057.java
+++ week01_20160919/src/hw/hw03_105021057.java
@@ -4,11 +4,24 @@ package hw;
  * Date: 2016/09/19
  * Author: 105021057 張岳埕
  */
+import java.util.Scanner;
 public class hw03_105021057 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		float a = scn.nextInt();
+		float b = scn.nextInt();
+		float c = scn.nextInt();
+		float d = scn.nextInt();
+		float e = scn.nextInt();
+		float f = scn.nextInt();
+		int x =Math.min(Math.round(a),Math.round(b));
+		x = Math.min(x,Math.round(c));
+		x = Math.min(x,Math.round(d));
+		x = Math.min(x,Math.round(e));
+		x = Math.min(x,Math.round(f));
+		System.out.println(x);
 	}
 
 }
